51478febfa9187afa813e94a743caacc1725511a
Gabriel Branco <71736817+brancogabriel@users.noreply.github.com>
2022-03-17 22:02:19 -0300

    implementacao login, logout e dahsboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,79 @@
-from django.shortcuts import render
-from django.contrib import messages
+import email
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+    
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    
+    user = auth.authenticate(request, username=usuario, password=senha)
+    
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos.')        
+        return render(request, 'accounts/login.html')
+    else:
+        auth.login(request, user)
+        messages.success(request, 'Login realizado com sucesso.')      
+        
+        return redirect('dashboard')  
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('index')
 
 def cadastro(request):
-    print(request.POST)
-    return render(request, 'accounts/cadastro.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/cadastro.html')
 
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+    
+    if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
+        messages.error(request, 'Todos os campos são obrigatórios')
+        return render(request, 'accounts/cadastro.html')
+        
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'Email inválido')
+        return render(request, 'accounts/cadastro.html')
+    
+    if len(senha) < 6:
+        messages.error(request, 'A senha deve 6 caracteres ou mais.')
+        return render(request, 'accounts/cadastro.html')
+
+    if len(usuario) < 6:
+        messages.error(request, 'O nome de usuário deve ter 6 caracteres ou mais.')
+        return render(request, 'accounts/cadastro.html')
+    
+    if senha!= senha2:
+        messages.error(request, 'Senhas não conferem.')
+        return render(request, 'accounts/cadastro.html')  
+    
+    if User.objects.filter(username=usuario).exists():
+        messages.error(request, 'Nome de usuário indisponível.')
+        return render(request, 'accounts/cadastro.html')  
+    
+    if User.objects.filter(email=email).exists():
+        messages.error(request, 'Email já existe.')
+        return render(request, 'accounts/cadastro.html')            
+    
+    messages.success(request, 'Usuário registrado com sucesso.')
+    
+    user = User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=sobrenome)
+    user.save()
+    return redirect('login')
+
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
